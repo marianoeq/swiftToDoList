99ae52d21e42e7316d49ecfdbb63f727f39e5ed6
Mariano Quiroga <mariano.quiroga@accenture.com>
2025-05-28 16:26:51 +0200

    first-commit

---
+++ SwiftToDoList/SwiftToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftToDoListApp.swift
+//  SwiftToDoList
+//
+//  Created by mariano.quiroga on 28/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
